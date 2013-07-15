cb3f90681cfe03d4d567d738166bb91afbdc7bcc
Jack Carrig <jack@getperka.com>
2013-07-15 12:06:51 -0400

    add js type Date

--- fast/src/main/java/com/getperka/flatpack/fast/JavaScriptDialect.java
+++ fast/src/main/java/com/getperka/flatpack/fast/JavaScriptDialect.java
@@ -218,6 +218,11 @@ public class JavaScriptDialect implements Dialect {
       return packageName + "." + upcase(type.getName());
     }
 
+    if (type.getTypeHint() != null
+      && type.getTypeHint().getValue().equals("org.joda.time.DateTime")) {
+      return "Date";
+    }
+
     String jsType = "nil";
     switch (type.getJsonKind()) {
       case BOOLEAN:
